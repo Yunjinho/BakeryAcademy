15e576917ff20733ff9f6836152c7b716481cfea
Yunjinho <dbswlsgh1238@naver.com>
2023-07-14 17:27:18 +0900

    test : 카테고리 Insert Sql 구문 테스트

--- BakeryAcademy/src/main/java/com/example/myapp/product/dao/IProductRepository.java
+++ BakeryAcademy/src/main/java/com/example/myapp/product/dao/IProductRepository.java
@@ -1,5 +1,13 @@
 package com.example.myapp.product.dao;
 
-public interface IProductRepository {
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+import com.example.myapp.product.model.Product;
+
+@Repository
+@Mapper
+public interface IProductRepository {
+	Product selectProduct(int productId);
+	void insertProduct(Product product);
 }
