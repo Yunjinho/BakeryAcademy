4f9596d996848aca616a52d5fb336713e87c0834
Yunjinho <dbswlsgh1238@naver.com>
2023-07-18 16:45:04 +0900

    feat : 카테고리 추가 삭제 수정 조회 기능 추가 FE는 추가 보완 필요

--- BakeryAcademy/src/main/java/com/example/myapp/product/controller/ProductController.java
+++ BakeryAcademy/src/main/java/com/example/myapp/product/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.myapp.product.dao.ICategoryRepository;
+import com.example.myapp.product.model.Category;
 import com.example.myapp.product.model.Product;
 import com.example.myapp.product.service.IProductService;
 
@@ -22,19 +23,20 @@ public class ProductController {
 	@Autowired
 	IProductService productService;
 	
+	
+	//상품 등록페이지로 이동
 	@RequestMapping(value="/admin/insert-product",method=RequestMethod.GET)
 	public String productInsert(Model model) {
 		model.addAttribute("category", categoryService.selectAllCategory());
-		model.addAttribute("header", "header");
 		return "admin/product-register";
 	}
-	
+	// 상품 등록 처리
 	@RequestMapping(value="/admin/insert-product",method=RequestMethod.POST)
 	public String productInsert(@Validated Product product,Model model) {
 		productService.insertProduct(product);
 		return "redirect:/";
 	}
-	
+	//상품 업데이트 페이지로 이동
 	@RequestMapping(value="/admin/update-product",method=RequestMethod.GET)
 	public String productUpdate(@RequestParam int productId, Model model) {
 		Product product=productService.selectProduct(productId);
@@ -42,10 +44,52 @@ public class ProductController {
 		model.addAttribute("product", product);
 		return "admin/product-update";
 	}
-
+	//상품 업데이트 처리
 	@RequestMapping(value="/admin/update-product",method=RequestMethod.POST)
 	public String productUpdate(@Validated Product product, Model model) {
 		productService.updateProduct(product);
 		return "redirect:/";
 	}
+	//카테고리 목록 리스트 페이지로 이동
+	@RequestMapping(value="/admin/category", method=RequestMethod.GET)
+	public String categoryView(Model model) {
+		model.addAttribute("categoryList", categoryService.selectAllCategory());
+		return "admin/category-view";
+	}
+	//카테고리 등록 페이지로 이동
+	@RequestMapping(value="/admin/insert-category", method=RequestMethod.GET)
+	public String categoryInsert() {
+		return "admin/category-register";
+	}
+	//카테고리 등록
+	@RequestMapping(value="/admin/insert-category",method=RequestMethod.POST)
+	public String categoryInsert(@Validated Category category,Model model) {
+		categoryService.insertCategory(category);
+		model.addAttribute("categoryList", categoryService.selectAllCategory());
+		return "redirect:/admin/category";
+	}
+	//카테고리 업데이트 페이지로 이동
+	@RequestMapping(value="/admin/update-category", method=RequestMethod.GET)
+	public String categoryUpdate(@RequestParam int categoryId,Model model) {
+		Category category=new Category();
+		category=categoryService.selectCategory(categoryId);
+		model.addAttribute("category", category);
+		return "admin/category-update";
+	}
+	//카테고리 업데이트
+	@RequestMapping(value="/admin/update-category",method=RequestMethod.POST)
+	public String categoryUpdate(@Validated Category category,Model model) {
+		categoryService.updateCategory(category);
+		model.addAttribute("categoryList", categoryService.selectAllCategory());
+		return "redirect:/admin/category";
+	}
+	
+	//카테고리 삭제
+	@RequestMapping(value="/admin/delete-category",method=RequestMethod.GET)
+	public String categoryDelete(@RequestParam int categoryId,Model model) {
+		categoryService.deleteCategory(categoryId);
+		model.addAttribute("categoryList", categoryService.selectAllCategory());
+		return "redirect:/admin/category";
+	}
+	
 }
