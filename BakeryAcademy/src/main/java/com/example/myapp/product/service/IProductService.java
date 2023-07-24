1707c8a6bee11ce5f23bbeffebbe362638d8d56f
SongARock <tmddn2278@naver.com>
2023-07-24 09:30:37 +0900

    chore : 중간 저장

--- BakeryAcademy/src/main/java/com/example/myapp/product/service/IProductService.java
+++ BakeryAcademy/src/main/java/com/example/myapp/product/service/IProductService.java
@@ -2,25 +2,31 @@ package com.example.myapp.product.service;
 
 import java.util.List;
 
-
-import java.util.List;
-
 import com.example.myapp.product.model.Product;
 import com.example.myapp.product.model.ProductImage;
 
 public interface IProductService {
 	public void insertProduct(Product product);
+
 	public void updateProduct(Product product);
+
 	public Product selectProduct(int productId);
-	
-	public List<Product> selectSearchKeywordProduct(String keyword,int start,int end);
-	public List<Product> selectProductAtModal(int start,int end);
-	List<Product> selectProductListInBaord(List<Integer> list);
-	
-	int countProductList();
-	int countKeyWordProductList(String keyword);
-	
+
+	public List<Product> selectSearchKeywordProduct(String keyword, int start, int end);
+
+	public List<Product> selectProductAtModal(int start, int end);
+
+	public List<Product> selectProductListInBaord(List<Integer> list);
+
+	public int countProductList();
+
+	public int countKeyWordProductList(String keyword);
+
 	public List<Product> selectProductListByCategory(int categoryId, int page);
+
 	public int selectTotalProductCountByCategory(int categoryId);
-	ProductImage getProductThumbnail(int productId);
+
+	public ProductImage getProductThumbnail(int productId);
+
+	public List<ProductImage> getProductImageList(int productId);
 }
