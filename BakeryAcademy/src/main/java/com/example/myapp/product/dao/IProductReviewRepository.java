e24f2c13a66c731281098bff078aa323b3543de1
SongARock <tmddn2278@naver.com>
2023-07-26 18:02:12 +0900

    chore : 상품 상세 페이지 댓글 출력 중

--- BakeryAcademy/src/main/java/com/example/myapp/product/dao/IProductReviewRepository.java
+++ BakeryAcademy/src/main/java/com/example/myapp/product/dao/IProductReviewRepository.java
@@ -1,5 +1,7 @@
 package com.example.myapp.product.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 //import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,6 @@ import com.example.myapp.product.model.ProductReview;
 @Mapper
 public interface IProductReviewRepository {
 	void insertProductReview(ProductReview productReview);
+	int selectProductReviewCount(int productId);
+	List<ProductReview> selectAllReviewByProductId(int productId);
 }
